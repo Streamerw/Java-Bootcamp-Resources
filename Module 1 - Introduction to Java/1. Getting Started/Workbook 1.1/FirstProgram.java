28cc15596f0252753cee8bb61a024a959b43c091
Streamerw <54499308+Streamerw@users.noreply.github.com>
2022-11-11 11:23:08 +0000

    1.gettingStarted

--- Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
+++ Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
@@ -1,5 +1,7 @@
-public class FirstProgramSolution{
-    public static void main(String[] args){
+public class FirstProgram 
+{
+    public static void main(String[] args)
+    {
         System.out.println("Printing is fun");
         System.out.println("Java > Python");
         System.out.println("Java all over the paper");
